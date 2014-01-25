8ed3e68cab48b4c770f83c025f476b567dcef46b
Juan Reyero <juan@juanreyero.com>
2014-01-25 18:39:14 +0100

    First version

---
+++ ox-leanpub.el
@@ -0,0 +1,177 @@
+;;; ox-leanpub.el --- Leanpub Markdown Back-End for Org Export Engine
+
+;; Author: Juan Reyero <juan _at! juanreyero.com>
+;; Keywords: org, wp, markdown, leanpub
+
+;;; Commentary:
+
+;;; Small adaptation of ox-md.el to make the exported markdown work
+;;; better for Leanpub (http://leanpub.com) publication.  It handles
+;;; footnotes, and makes source code separated from its output, and
+;;; the output does not display line numbers.  Html blocks are
+;;; ignored.
+
+;;; Missing:
+
+;;; - Tables should appear just as they are in org-mode.  Currently
+;;;   they are ignored.
+;;; - When using IDs for cross linking they should be taken advantage
+;;;   of.  Leanpub's markdown is supposed to be able to crosslink with
+;;;   ids.
+
+;;; Code:
+
+(eval-when-compile (require 'cl))
+(require 'ox-md)
+
+;;; Define Back-End
+
+(org-export-define-derived-backend 'leanpub 'md
+  :export-block '("leanpub" "LEANPUB")
+  :menu-entry
+  '(?L "Export to Leanpub Markdown"
+       ((?L "To temporary buffer"
+	    (lambda (a s v b) (org-leanpub-export-as-markdown a s v)))
+	(?l "To file" (lambda (a s v b) (org-leanpub-export-to-markdown a s v)))
+	(?o "To file and open"
+	    (lambda (a s v b)
+	      (if a (org-leanpub-export-to-markdown t s v)
+		(org-open-file (org-leanpub-export-to-markdown nil s v)))))))
+  :translate-alist '((fixed-width . org-leanpub-fixed-width-block)
+                     (src-block . org-leanpub-src-block)
+                     (plain-text . org-leanpub-plain-text)
+                     (inner-template . org-leanpub-inner-template)
+                     (footnote-reference . org-leanpub-footnote-reference)
+                     (headline . org-leanpub-headline)
+                     (table . org-leanpub-ignore)
+                     ;; Will not work with leanpub:
+                     (export-block . org-leanpub-ignore))) ; #+html
+
+
+;;; Adding the id, hoping to make crosslinks work at some point.
+;;; So far it is useless.
+(defun org-leanpub-headline (headline contents info)
+  (concat (let ((id (org-element-property :ID headline)))
+            (if id
+                (format "{#%s}\n" id)
+              ""))
+          (org-md-headline headline contents info)))
+
+;;; Required to make footnotes work.
+(defun org-leanpub-inner-template (contents info)
+  "Return complete document string after markdown conversion.
+CONTENTS is the transcoded contents string.  INFO is a plist
+holding export options."
+  (concat
+   contents
+   (let ((definitions (org-export-collect-footnote-definitions
+                       (plist-get info :parse-tree) info)))
+     (mapconcat (lambda (ref)
+                  (let ((id (format "[^%s]: " (let ((label (cadr ref)))
+                                                (if label
+                                                    label
+                                                  (car ref))))))
+                    (let ((def (nth 2 ref)))
+                      (concat id (org-export-data def info)))))
+                definitions "\n\n"))))
+
+(defun org-leanpub-footnote-reference (footnote contents info)
+  (format "[^%s]"
+          (let ((label (org-element-property :label footnote)))
+            (if label
+                label
+              (org-export-get-footnote-number footnote info)))))
+
+(defun org-leanpub-ignore (src-block contents info)
+  "")
+
+(defun org-leanpub-plain-text (text info)
+  text)
+
+;;; {lang="python"}
+;;; ~~~~~~~~
+;;; def longitude_circle(diameter):
+;;;     return math.pi * diameter
+;;; longitude(10)
+;;; ~~~~~~~~
+(defun org-leanpub-src-block (src-block contents info)
+  "Transcode SRC-BLOCK element into Markdown format.
+CONTENTS is nil.  INFO is a plist used as a communication
+channel."
+  (let ((lang (org-element-property :language src-block)))
+    (format "{lang=\"%s\"}\n~~~~~~~~\n%s~~~~~~~~"
+            lang
+            (org-remove-indentation
+             (org-element-property :value src-block)))))
+
+;;; A> {linenos=off}
+;;; A> ~~~~~~~~
+;;; A> 123.0
+;;; A> ~~~~~~~~
+(defun org-leanpub-fixed-width-block (src-block contents info)
+  "Transcode FIXED-WIDTH-BLOCK element into Markdown format.
+CONTENTS is nil.  INFO is a plist used as a communication
+channel."
+  (replace-regexp-in-string
+   "^" "A> "
+   (format "{linenos=off}\n~~~~~~~~\n%s~~~~~~~~"
+           (org-remove-indentation
+            (org-element-property :value src-block)))))
+
+;;; Interactive function
+
+;;;###autoload
+(defun org-leanpub-export-as-markdown (&optional async subtreep visible-only)
+  "Export current buffer to a Markdown buffer.
+
+If narrowing is active in the current buffer, only export its
+narrowed part.
+
+If a region is active, export that region.
+
+A non-nil optional argument ASYNC means the process should happen
+asynchronously.  The resulting buffer should be accessible
+through the `org-export-stack' interface.
+
+When optional argument SUBTREEP is non-nil, export the sub-tree
+at point, extracting information from the headline properties
+first.
+
+When optional argument VISIBLE-ONLY is non-nil, don't export
+contents of hidden elements.
+
+Export is done in a buffer named \"*Org MD Export*\", which will
+be displayed when `org-export-show-temporary-export-buffer' is
+non-nil."
+  (interactive)
+  (org-export-to-buffer 'leanpub "*Org LEANPUB Export*"
+    async subtreep visible-only nil nil (lambda () (text-mode))))
+
+;;;###autoload
+(defun org-leanpub-export-to-markdown (&optional async subtreep visible-only)
+  "Export current buffer to a Markdown file.
+
+If narrowing is active in the current buffer, only export its
+narrowed part.
+
+If a region is active, export that region.
+
+A non-nil optional argument ASYNC means the process should happen
+asynchronously.  The resulting file should be accessible through
+the `org-export-stack' interface.
+
+When optional argument SUBTREEP is non-nil, export the sub-tree
+at point, extracting information from the headline properties
+first.
+
+When optional argument VISIBLE-ONLY is non-nil, don't export
+contents of hidden elements.
+
+Return output file's name."
+  (interactive)
+  (let ((outfile (org-export-output-file-name ".md" subtreep)))
+    (org-export-to-file 'leanpub outfile async subtreep visible-only)))
+
+(provide 'ox-leanpub)
+
+;;; ox-leanpub.el ends here
